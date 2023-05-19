988e734275ab02edd01a3b2451f53a12a518237d
Barry Hanna <barryahanna@gmail.com>
2023-05-19 10:41:51 +0100

    Conditionally render inputs for selected system

--- src/components/BMICalculator/BMICalculator.tsx
+++ src/components/BMICalculator/BMICalculator.tsx
@@ -1,5 +1,8 @@
+import React from 'react';
+
 const BMICalculator = () => {
     // TODO: Add state and control inputs
+    const [metric, setMetric] = React.useState(true);
     // Conditionally render inputs for selected measurement system
     return <form>
         <h2>Enter your details below</h2>
@@ -9,14 +12,34 @@ const BMICalculator = () => {
             <label htmlFor="imperial">Imperial</label>
             <input name="measurement" type="radio" id="imperial" value="imperial"/>
         </fieldset>
-        <fieldset>
-            <p>Height</p>
-            <input type="number" name="" id="height" inputMode="numeric"/>
-        </fieldset>
-        <fieldset>
-            <p>Weight</p>
-            <input type="number" name="" id="height" inputMode="numeric"/>
-        </fieldset>
+       {metric && (
+        <>
+            <fieldset>
+                <p>Height</p>
+                <input type="number" name="height-cm" id="height" inputMode="numeric"/>
+            </fieldset>
+            <fieldset>
+                <p>Weight</p>
+                <input type="number" name="weight-kg" id="height" inputMode="numeric"/>
+            </fieldset></>
+
+            )
+        }
+       {!metric && (
+        <>
+            <fieldset>
+                <p>Height</p>
+                <input type="number" name="height-ft" id="height" inputMode="numeric"/>
+                <input type="number" name="height-in" id="height" inputMode="numeric"/>
+            </fieldset>
+            <fieldset>
+                <p>Weight</p>
+                <input type="number" name="weight-st" id="height" inputMode="numeric"/>
+                <input type="number" name="weight-lbs" id="height" inputMode="numeric"/>
+            </fieldset>
+            </>
+            )
+        }
         
         <div className="results__panel">
             <p>Your BMI is...</p>
