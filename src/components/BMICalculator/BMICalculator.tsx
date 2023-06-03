1e5e18a4baffbe1d0ddf365642f63fd330494e4b
Barry Hanna <barryahanna@gmail.com>
2023-06-03 21:38:50 +0100

    Add header styles

--- src/components/BMICalculator/BMICalculator.tsx
+++ src/components/BMICalculator/BMICalculator.tsx
@@ -10,39 +10,39 @@ const BMICalculator = () => {
         <fieldset className={styles.measurementSelectionContainer}>
             <input name="unit" type="radio" id="metric" value="metric" checked />
             <label htmlFor="metric">Metric</label>
-            <input name="unit" type="radio" id="imperial" value="imperial"/>
+            <input name="unit" type="radio" id="imperial" value="imperial" />
             <label htmlFor="imperial">Imperial</label>
         </fieldset>
-       {metric && (
-        <>
-            <fieldset>
-                <p>Height</p>
-                <input type="number" name="height-cm" id="height" inputMode="numeric"/>
-            </fieldset>
-            <fieldset>
-                <p>Weight</p>
-                <input type="number" name="weight-kg" id="height" inputMode="numeric"/>
-            </fieldset></>
+        {metric && (
+            <>
+                <fieldset>
+                    <div>Height</div>
+                    <input type="number" name="height-cm" id="height" inputMode="numeric" />
+                </fieldset>
+                <fieldset>
+                    <div>Weight</div>
+                    <input type="number" name="weight-kg" id="height" inputMode="numeric" />
+                </fieldset></>
 
-            )
+        )
         }
-       {!metric && (
-        <>
-            <fieldset>
-                <p>Height</p>
-                <input type="number" name="height-ft" id="height" inputMode="numeric"/>
-                <input type="number" name="height-in" id="height" inputMode="numeric"/>
-            </fieldset>
-            <fieldset>
-                <p>Weight</p>
-                <input type="number" name="weight-st" id="height" inputMode="numeric"/>
-                <input type="number" name="weight-lbs" id="height" inputMode="numeric"/>
-            </fieldset>
+        {!metric && (
+            <>
+                <fieldset>
+                    <p>Height</p>
+                    <input type="number" name="height-ft" id="height" inputMode="numeric" />
+                    <input type="number" name="height-in" id="height" inputMode="numeric" />
+                </fieldset>
+                <fieldset>
+                    <p>Weight</p>
+                    <input type="number" name="weight-st" id="height" inputMode="numeric" />
+                    <input type="number" name="weight-lbs" id="height" inputMode="numeric" />
+                </fieldset>
             </>
-            )
+        )
         }
-        
-        <div className="results__panel">
+
+        <div className={styles.resultsPanel}>
             <p>Your BMI is...</p>
             <p className="results__result">23.4</p>
             <p>Your BMI suggests you're <span>a healthy weight.</span> our ideal weight is between <span>63.3kgs - 85.2kgs</span></p>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,15 @@
 import BMICalculator from '../BMICalculator'
 
 const Header = () => {
-    return <>
-        <h1>Body Mass Index Calculator</h1>
-        <p>Better understand your weight in relation to your height using our 
-  body mass index (BMI) calculator. While BMI is not the sole determinant 
-  of a healthy weight, it offers a valuable starting point to evaluate 
-  your overall health and well-being.</p>
+    return <header className='header flow'>
+        <img className="logo" src="/assets/images/logo.svg" alt="" />
+        <h1>Body&nbsp;Mass Index Calculator</h1>
+        <p className='intro-text'>Better understand your weight in relation to your height using our
+            body mass index (BMI) calculator. While BMI is not the sole determinant
+            of a healthy weight, it offers a valuable starting point to evaluate
+            your overall health and well-being.</p>
         <BMICalculator />
-    </>
+    </header>
 }
 
 export default Header;
