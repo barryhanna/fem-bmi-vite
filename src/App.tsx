4e11e836b9856a84cedfdbf02b948e02a52de7ac
Barry Hanna <barryahanna@gmail.com>
2023-06-04 09:56:12 +0100

    Style header background

--- src/App.tsx
+++ src/App.tsx
@@ -3,52 +3,55 @@ import Header from './components/Header';
 import ImproveBMICard from './components/ImproveBMICard/ImproveBMICard';
 
 function App() {
-  
+
 
   return (
     <>
       <Header />
-      <div className="improve-bmi__card-list">
-        <ImproveBMICard icon="eating">
+      <section className="improve-bmi">
+        <img src="/assets/images/image-man-eating.webp" alt="A man smiling whilst eating sushi with chopsticks" />
+        <div className="improve-bmi__card-list">
+          <ImproveBMICard icon="eating">
             <h2>Healthy eating</h2>
             <p>Healthy eating promotes weight control, disease prevention, better digestion, immunity, mental clarity, and mood.</p>
           </ImproveBMICard>
-        <ImproveBMICard icon="exercise">
+          <ImproveBMICard icon="exercise">
             <h2>Regular exercise</h2>
             <p>Exercise improves fitness, aids weight control, elevates mood, and reduces disease
-risk, fostering wellness and longevity.</p>
+              risk, fostering wellness and longevity.</p>
           </ImproveBMICard>
-        <ImproveBMICard icon="sleep">
+          <ImproveBMICard icon="sleep">
             <h2>Adequate sleep</h2>
             <p>Sleep enhances mental clarity, emotional stability, and physical wellness, promoting
-overall restoration and rejuvenation.</p>
+              overall restoration and rejuvenation.</p>
           </ImproveBMICard>
-        
-      </div>
+
+        </div>
+      </section>
       <section className="limitations-of-bmi">
         <BMILimitationCard icon="gender" title="Gender">
           <p>The development and body fat composition of girls and boys vary with age. Consequently,
-a child's age and gender are considered when evaluating their BMI.</p>
+            a child's age and gender are considered when evaluating their BMI.</p>
         </BMILimitationCard>
         <BMILimitationCard icon="age" title="Age">
           <p>In aging individuals, increased body fat and muscle loss may cause BMI to underestimate
-body fat content.</p>
+            body fat content.</p>
         </BMILimitationCard>
         <BMILimitationCard icon="muscle" title="Muscle">
           <p>BMI may misclassify muscular individuals as overweight or obese, as it doesn't
-differentiate muscle from fat.</p>
+            differentiate muscle from fat.</p>
         </BMILimitationCard>
         <BMILimitationCard icon="pregnancy" title="Pregnancy">
           <p>Expectant mothers experience weight gain due to their growing baby. Maintaining a
-healthy pre-pregnancy BMI is advisable to minimise health risks for both mother
-and child.</p>
+            healthy pre-pregnancy BMI is advisable to minimise health risks for both mother
+            and child.</p>
         </BMILimitationCard>
         <BMILimitationCard icon="race" title="Race">
           <p>Certain health concerns may affect individuals of some Black and Asian origins at
-lower BMIs than others. To learn more, it is advised to discuss this with your
-GP or practice nurse.</p>
+            lower BMIs than others. To learn more, it is advised to discuss this with your
+            GP or practice nurse.</p>
         </BMILimitationCard>
-        
+
       </section>
     </>
   )
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,18 @@
 import BMICalculator from '../BMICalculator'
 
 const Header = () => {
-    return <header className='header flow'>
-        <img className="logo" src="/assets/images/logo.svg" alt="" />
-        <h1>Body&nbsp;Mass Index Calculator</h1>
-        <p className='intro-text'>Better understand your weight in relation to your height using our
-            body mass index (BMI) calculator. While BMI is not the sole determinant
-            of a healthy weight, it offers a valuable starting point to evaluate
-            your overall health and well-being.</p>
-        <BMICalculator />
-    </header>
+    return (
+        <header className='header flow'>
+            <div className="header-bg"></div>
+            <div className="header-bg-gradient"></div>
+            <img className="logo" src="/assets/images/logo.svg" alt="" />
+            <h1 className="header-heading">Body&nbsp;Mass Index Calculator</h1>
+            <p className='intro-text'>Better understand your weight in relation to your height using our
+                body mass index (BMI) calculator. While BMI is not the sole determinant
+                of a healthy weight, it offers a valuable starting point to evaluate
+                your overall health and well-being.</p>
+            <BMICalculator />
+        </header>)
 }
 
 export default Header;
