c2e048667c54a68fc6ad013cbe97431b65cd2258
Barry Hanna <barryahanna@gmail.com>
2023-06-06 21:45:22 +0100

    Add calculations

--- src/components/BMICalculator/BMICalculator.tsx
+++ src/components/BMICalculator/BMICalculator.tsx
@@ -7,12 +7,52 @@ const BMICalculator = () => {
     const [height, setHeight] = React.useState(160);
     const [weight, setWeight] = React.useState(80);
 
+    const calculateBMI = () => { }
+
+    const weightMetricToImperial = (kg: number) => {
+        // 1kg = 2.204623 lbs
+        // 1 stone = 14lbs
+        const totalPounds = kg * 2.204623;
+        const stone = Math.floor(totalPounds / 14);
+        const lbs = Math.round(totalPounds % stone);
+        console.log({ stone, lbs })
+        return { stone, lbs };
+    }
+
+    const heightMetricToImperial = (cm: number) => {
+        // 1cm = 0.3937008in
+        // 1ft = 12in
+        const totalInches = cm * 0.3937008;
+        const feet = Math.floor(totalInches / 12);
+        const inches = Math.round(totalInches % feet);
+        console.log({ feet, inches })
+        return { feet, inches }
+    }
+
     const handleUpdate = (e) => {
         const { name, value } = e.target;
-        if (name === "height-cm") {
-            setHeight(value)
-        } else if (name === "weight-kg") {
-            setWeight(value);
+
+        switch (name) {
+            case "height-cm":
+                setHeight(value)
+                break;
+            case "weight-kg":
+                setWeight(value);
+                break;
+            case "height-ft":
+                // calculate metric
+                break;
+            case "height-in":
+                // calculate metric
+                break;
+            case "weight-st":
+                // calculate metric
+                break;
+            case "weight-lbs":
+                // calculate metric
+                break;
+            default:
+
         }
     }
 
@@ -51,15 +91,15 @@ const BMICalculator = () => {
                 <div>Height</div>
                 <fieldset>
                     <div className='bmi-calculator--input-group-container'>
-                        <fieldset data-unit="ft"><input type="number" name="height-ft" id="height-ft" inputMode="numeric" onChange={handleUpdate} data-unit="ft" /></fieldset>
-                        <fieldset data-unit="in"><input type="number" name="height-in" id="height-in" inputMode="numeric" onChange={handleUpdate} data-unit="in" /></fieldset>
+                        <fieldset data-unit="ft"><input type="number" name="height-ft" id="height-ft" inputMode="numeric" onChange={handleUpdate} data-unit="ft" value={heightMetricToImperial(height).feet} /></fieldset>
+                        <fieldset data-unit="in"><input type="number" name="height-in" id="height-in" inputMode="numeric" onChange={handleUpdate} data-unit="in" value={heightMetricToImperial(height).inches} /></fieldset>
                     </div>
                 </fieldset>
                 <div>Weight</div>
                 <fieldset>
                     <div className='bmi-calculator--input-group-container'>
-                        <fieldset data-unit="st"><input type="number" name="weight-st" id="weight-st" inputMode="numeric" data-unit="st" /></fieldset>
-                        <fieldset data-unit="lbs"><input type="number" name="weight-lbs" id="weight-lbs" inputMode="numeric" data-unit="lbs" /></fieldset>
+                        <fieldset data-unit="st"><input type="number" name="weight-st" id="weight-st" inputMode="numeric" data-unit="st" value={weightMetricToImperial(weight).stone} /></fieldset>
+                        <fieldset data-unit="lbs"><input type="number" name="weight-lbs" id="weight-lbs" inputMode="numeric" data-unit="lbs" value={weightMetricToImperial(weight).lbs} /></fieldset>
                     </div>
                 </fieldset>
             </>
